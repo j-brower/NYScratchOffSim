bafdbf6e70ae337cffd42139743c6c21e70cd376
JaredDB <jbrower@oswego.edu>
2017-12-02 15:56:28 -0500

    see previous

--- script/select.js
+++ script/select.js
@@ -4,18 +4,18 @@ Vue.http.get('https://j-brower.github.io/NYScratchOffSim/tickets.json').then(res
 
     // get body data
     data = response.json();
+    console.log(data);
   }, response => {
     console.log(response);
     console.log("Could not retrieve data.");
   });
-console.log(data);
 var select = new Vue({
 
   el: '#select',
   data: {
     tickets: function() {
       ticketArray = [];
-      var jsonTicketArray = yourJSONObject.get("tickets");
+      var jsonTicketArray = data.get("tickets");
       for(i = 0; i < jsonTicketArray.length; i++) {
         console.log(jsonTicketArray[i]);
       }
