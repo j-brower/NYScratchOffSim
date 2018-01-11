f19a885fc51c54a6d3dbb06da2f8f4bb5103fc56
JaredDB <jbrower@oswego.edu>
2018-01-11 18:26:28 -0500

    it works

--- script/select.js
+++ script/select.js
@@ -15,7 +15,40 @@ function readBalance() {
 }
 
 function simulateScratch(ticket) {
-  return 1;
+  var wins = [];
+  for(var i = 0; i < ticket.chances.length; i++) {
+    if(isWinner(ticket.chances[i].chance)) {
+      wins.push(ticket.chances[i].prize);
+    }
+  }
+  if(wins.length > 1) { //pick a pseudorandom prize from winners
+    var index = Math.floor(Math.random() * Math.floor(wins.length));
+    console.log("won " + wins[index]);
+    if(wins[index] == "Free Take 5") {
+      return wins[index];
+    }
+    return wins[index] - ticket.price;
+  } else if(wins.length == 1) {
+    console.log("won " + wins[0]);
+    if(wins[0] == "Free Take 5") {
+      return wins[0];
+    }
+    return wins[0] - ticket.price;
+  }
+  return (ticket.price * -1);
+}
+function isWinner(chance) {
+  //TODO: figure out how to get numbers from random.org with axios
+
+  if((chance % 1) == 0) {
+    //console.log(chance + " has no decimal component");
+    return 1 == Math.floor(Math.random() * Math.floor(chance));
+  } else if(((chance * 10) % 1) == 0) {
+    return 10 > Math.floor(Math.random() * Math.floor(chance * 10));
+  } else if(((chance * 100) % 1) == 0) {
+    return 100 > Math.floor(Math.random() * Math.floor(chance * 100));
+  }
+  return false;
 }
 
 
@@ -26,9 +59,7 @@ if(isNaN(initialBalance)) {
   initialBalance = 0;
   document.cookie = "balance=0; expires=Sat, 01 Jan 2118 12:00:00 UTC; path=/";
 }
-
 axios.get('https://j-brower.github.io/NYScratchOffSim/tickets.json').then(response => {
-
   ticketsRes = response.data.tickets;
   for(i = 0; i < ticketsRes.length; i++) {
     var obj = {
@@ -66,17 +97,25 @@ var app = new Vue({
       this.selecting = false;
       for(var i = 0; i < this.tickets.length; i++) {
         if(this.tickets[i].quantity > 0) {
-          var result = 0;
           for(var j = 0; j < this.tickets[i].quantity; j++) {
-            result += simulateScratch(this.tickets[i]);
+            var result = 0;
+            result = simulateScratch(this.tickets[i]);
+            var resultTicket = {};
+            resultTicket.price = this.tickets[i].price;
+            resultTicket.imgURL = this.tickets[i].imgURL;
+            resultTicket.result = result;
+            this.resultsArray.push(resultTicket);
+            if(result != "Free Take 5") {
+              this.balance += result;
+            } else { //deduct cost of ticket for quickpick
+              this.balance -= 1;
+            }
           }
-          var resultTicket = this.tickets[i];
-          resultTicket.result = result;
-          this.resultsArray.push(resultTicket);
-          this.balance += result;
         }
       }
       this.showingResults = true;
+      document.cookie = "balance=" + this.balance +
+        "; expires=Sat, 01 Jan 2118 12:00:00 UTC; path=/";
     },
     endResults: function(event) {
       //alert("not implemented");
@@ -85,8 +124,7 @@ var app = new Vue({
       for(var i=0; i<this.tickets.length; i++) {
         this.tickets[i].quantity = 0;
       }
-      document.cookie = "balance=" + this.balance +
-        "; expires=Sat, 01 Jan 2118 12:00:00 UTC; path=/";
+      this.resultsArray = [];
     }
   }
 
